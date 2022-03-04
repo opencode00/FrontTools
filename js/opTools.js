f6237abc08a48666630a4870ddec6d6704c21619
Pedro Hdez <opencode00@gmail.com>
2022-03-04 16:57:10 +0000

    Cambios 2

--- js/opTools.js
+++
@@ -1,7 +0,0 @@
-function include(file, querySelectorElement){
-    fetch(file).then(result => result.text()).then(content => {
-        let element = document.querySelector(querySelectorElement);
-        element.innerHTML = content;
-    });
-}
-
